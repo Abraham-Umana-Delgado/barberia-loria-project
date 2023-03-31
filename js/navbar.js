6786d9a29e1a9ab02452d3abff46f2c944c65b0d
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-03-30 22:09:58 -0600

    Updated

--- js/navbar.js
+++ js/navbar.js
@@ -4,4 +4,25 @@ const navWrapper = document.getElementById('nav')
 toggleButton.addEventListener('click',() => {
   toggleButton.classList.toggle('close')
   navWrapper.classList.toggle('show')
+});
+
+const menuLinks = document.querySelectorAll('.navbar-nav--menu a[href^=\"#\"]');
+
+menuLinks.forEach(menuLink => {
+  menuLink.addEventListener("click", function() {
+    navWrapper.classList.remove('show');
+    toggleButton.classList.toggle('close')
+  })
 })
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    nav.classList.add('black-background');
+    nav.classList.remove('transparent');
+  } else {
+    nav.classList.remove('black-background');
+    nav.classList.add('transparent');
+  }
+});
