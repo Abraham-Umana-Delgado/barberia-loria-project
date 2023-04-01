3f6d5dbea523f4786e5d21c8981f98f503f7ddd3
JeimmyL <89615089+Jeim05@users.noreply.github.com>
2023-03-31 22:43:06 -0600

    Update read-more-about.js

--- js/read-more-about.js
+++ js/read-more-about.js
@@ -3,6 +3,12 @@ const hide_text = document.getElementById('hideTxt');
 
 btn_read.addEventListener('click', hideText);
 
-function hideText(){
-    hide_text.classList.toggle('show')
+function hideText() {
+    hide_text.classList.toggle('show');
+
+    if (hide_text.classList.contains('show')) {
+        btn_read.innerHTML = 'Leer menos';
+    } else {
+        btn_read.innerHTML = 'Leer más';
+    }
 }
