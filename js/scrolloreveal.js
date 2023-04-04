29dd413657ceedb1632d2a2ce457c2b4724290a0
Abraham-Umana-Delgado <info@abrahamumanadelgado.com>
2023-04-04 16:19:19 -0600

    Update

--- js/scrolloreveal.js
+++ js/scrolloreveal.js
@@ -1,4 +1,4 @@
-ScrollReveal();
+/*ScrollReveal();
 
 ScrollReveal().reveal('.reveal', {
     delay: 80,
@@ -6,4 +6,21 @@ ScrollReveal().reveal('.reveal', {
     origin: 'bottom',
     easing: 'ease-out',
     reset: true
-});
+});*/
+
+// registramos ScrollTrigger como un plugin de GSAP
+/*gsap.registerPlugin(ScrollTrigger);
+
+// creamos la animación
+gsap.from(".fade-in", {
+  opacity: 0, // empezamos con opacidad 0 para que el elemento esté oculto al principio
+  duration: 1.5, // duración de la animación en segundos
+  ease: "power2.out", // tipo de curva de aceleración de la animación
+  scrollTrigger: { // configuramos el trigger de ScrollTrigger para que la animación se active cuando el usuario llegue al elemento
+    trigger: ".fade-in",
+    start: "top 80%", // activamos la animación cuando el elemento llega al 80% de la pantalla
+    end: "bottom 20%", // desactivamos la animación cuando el elemento sale del 20% inferior de la pantalla
+    toggleActions: "play none none reset" // configuramos el comportamiento de la animación cuando el usuario se desplaza hacia atrás
+  }
+});*/
+
