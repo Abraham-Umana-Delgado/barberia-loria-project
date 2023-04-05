e47586194639d85cf0a963f4b6299ea8696b7a96
Abraham-Umana-Delgado <info@abrahamumanadelgado.com>
2023-04-04 20:37:39 -0600

    Updated

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 const toggleButton = document.getElementById('nav_menu')
 const navWrapper = document.getElementById('nav')
 
-toggleButton.addEventListener('click',() => {
+toggleButton.addEventListener('click', () => {
   toggleButton.classList.toggle('close')
   navWrapper.classList.toggle('show')
 });
@@ -9,20 +9,8 @@ toggleButton.addEventListener('click',() => {
 const menuLinks = document.querySelectorAll('.navbar-nav--menu a[href^=\"#\"]');
 
 menuLinks.forEach(menuLink => {
-  menuLink.addEventListener("click", function() {
+  menuLink.addEventListener("click", function () {
     navWrapper.classList.remove('show');
     toggleButton.classList.toggle('close')
   })
 })
-
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    nav.classList.add('black-background');
-    nav.classList.remove('transparent');
-  } else {
-    nav.classList.remove('black-background');
-    nav.classList.add('transparent');
-  }
-});
