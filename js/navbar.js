9f1958fb1b3941e506d0b4225dde980dede4cd98
Abraham-Umana-Delgado <info@abrahamumanadelgado.com>
2023-04-12 10:30:42 -0600

    Updated

--- js/navbar.js
+++ js/navbar.js
@@ -18,7 +18,7 @@ menuLinks.forEach(menuLink => {
 const nav = document.querySelector('nav');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 5) {
+  if (window.scrollY > 0.1) {
     nav.classList.add('black-background');
     nav.classList.remove('transparent');
   } else {
